f94fbb4c61497fdf2703dcb6b86d9709c963631a
Mohammed Khairul Islam <khairulalvi@gmail.com>
2017-01-21 18:06:17 -0500

    Created a working prototype for syntax highlighting. Dynamically makes the word int bold as you type.

--- app/codeEditor/codeEditorController.js
+++ app/codeEditor/codeEditorController.js
@@ -2,5 +2,5 @@ angular.module('snow')
 	.controller('CodeEditorController', ['$scope', CodeEditorController]);
 
 function CodeEditorController($scope) {
-	$scope.foo = "int <b> MEOW </b> ASDFASDF";
+	$scope.code = ""
 }
--- app/codeEditor/codeEditorDirective.js
+++ app/codeEditor/codeEditorDirective.js
@@ -1,6 +1,50 @@
+var angular2 = require('angular')
 angular.module('snow')
 	.directive('codeFull', function () {
+		function makeIntsBold(pureCodeString) {
+			return pureCodeString.replace(/int/g, "<b>int</b>");
+		}
+
+		function filterBold(formattedCode) {
+			return formattedCode.replace(/<b>|<\/b>/g, "");
+		}
 		return {
-			templateUrl: 'codeEditor/codeEditor.html'
+			require: 'ngModel',
+			link: function (scope, elem, attrs, mdlCtrlr) {
+				console.log(scope);
+				console.log(mdlCtrlr);
+				mdlCtrlr.$render = function () {
+					elem.html(mdlCtrlr.$viewValue); //Inject Html String as Html into Div
+				}
+				elem.on('keyup blur paste', function () {
+					var currentViewValue = elem.html();
+					mdlCtrlr.$setViewValue(currentViewValue)
+				});
+
+				//Necessary to update View again due to View changing model which changes view again.Hence we run all formatters.
+				function runFormatters(ctrl) {
+					// this function is a copy of the internal formatter running code.
+					// https://github.com/angular/angular.js/issues/3407#issue-17469647
+					var modelValue = ctrl.$modelValue;
+					var formatters = ctrl.$formatters;
+					var idx = formatters.length;
+					var viewValue = modelValue;
+					while (idx--) {
+						viewValue = formatters[idx](viewValue);
+					}
+					if (ctrl.$viewValue !== viewValue) {
+						ctrl.$viewValue = ctrl.$$lastCommittedViewValue = viewValue;
+						ctrl.$render();
+						ctrl.$$runValidators(modelValue, viewValue, angular.noop);
+					}
+				}
+				scope.$watch("code", function (newValue, oldValue) {
+					runFormatters(mdlCtrlr);
+				});
+				mdlCtrlr.$formatters.push(makeIntsBold);
+				mdlCtrlr.$parsers.push(filterBold);
+				// elem.on('blur keyup paste', )
+			}
+
 		}
 	})
