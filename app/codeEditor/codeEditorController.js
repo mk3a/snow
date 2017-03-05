01af287d21b20f9f3472ab8d61d0539696cb824e
Mohammed Khairul Islam <khairulalvi@gmail.com>
2017-03-04 23:13:15 -0500

    Fixed status bar added and horizontal scrolling added.

--- app/codeEditor/codeEditorController.js
+++ app/codeEditor/codeEditorController.js
@@ -3,5 +3,5 @@ var angular = require('angular');
 
 module.exports = function CodeEditorController($scope) {
 	// $scope.code = '/*THIS IS A COMMENT\nabc';
-	$scope.code = '/*#include<iostream.h>\n\nint main(){\n//THIS IS A COMMENT\n\tfloat a = 45.23;\n\t*/\ncout<<a<<"Hello World";\n}';
+	$scope.code = '/*#include<iostream.h>\n\nint main(){\n//THIS IS A COMMENT\n\tfloat a = 45.23;\n\t*/\ncout<<a<<"Hello World";cout<<a<<"Hello World";cout<<a<<"Hello World";cout<<a<<"Hello World";\n}';
 }
